d94b28287fe623f73b17d2139116d434dd8a80bf
unknown <Fireostendere@gmail.com>
2024-02-24 15:19:13 +0100

    My changes

--- conftest.py
+++ conftest.py
@@ -4,20 +4,22 @@ from fixture.application import Application
 fixture = None
 
 
-@pytest.fixture
-def app():
+@pytest.fixture(scope="session")
+def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
         # Проверка на наличие фикстуры и, если её нет, создание заново
         practice_form_page = fixture.open_practice_form_page()
         practice_form_page.open_site()
-    return fixture
 
-
-@pytest.fixture(scope="session", autouse=True)
-def stop(request):
     def fin():
         fixture.destroy()
     request.addfinalizer(fin)
-    return fixture
+    yield fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request, app):
+    yield app
+
